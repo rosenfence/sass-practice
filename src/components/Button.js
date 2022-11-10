3b3b4cce1b2ee0e415b78a2d67f6d590aa0c7175
rosenfence <rosenfence@gmail.com>
2022-11-11 00:22:53 +0900

    SCSS Practice

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import classNames from 'classnames';
 import './Button.scss';
 
-const Button = ({ children }) => {
-  return <button className='Button'>{children}</button>;
+const Button = ({ children, size, color, outline, fullWidth, ...rest }) => {
+  return (
+    <button className={classNames('Button', size, color, { outline, fullWidth })} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  size: 'medium',
+  color: 'blue',
 };
 
 export default Button;
